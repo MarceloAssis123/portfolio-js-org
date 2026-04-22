7caa2daa3527a4565c758d85f2203264c2348034
Marcelo Miguel Pereira de Assis <124265415+MarceloAssis123@users.noreply.github.com>
2026-04-22 09:24:45 -0300

    feat: add business

---
+++ business.js
@@ -0,0 +1,37 @@
+const business = [
+    {
+        Name: "PhotoGen",
+        Website: "https://photogen.com.br",
+        Logo: "https://photogen.com.br/PhotoGenLogo500x100.svg"
+    },
+    {
+        Name: "Interior AI",
+        Website: "https://interiorai.com.br/",
+        Logo: "https://interiorai.com.br/Logo500x100.svg"
+    },
+    {
+        Name: "Papagaio AI",
+        Website: "https://papagaioai.com/",
+        Logo: "https://papagaioai.com/Logo500x100.svg"
+    },
+    {
+        Name: "Niverfy",
+        Website: "https://niverfy.com/",
+        Logo: "https://niverfy.com/Logo500x100.svg"
+    },
+    {
+        Name: "Pergunta Pix",
+        Website: "https://perguntapix.com/",
+        Logo: "https://perguntapix.com/Logo500x100.svg"
+    },
+    {
+        Name: "Influenciador IA",
+        Website: "https://influenciadoria.com/",
+        Logo: "https://influenciadoria.com/Logo500x100.svg"
+    },
+    {
+        Name: "Rastreia Receita",
+        Website: "https://rastreiareceita.com/",
+        Logo: "https://rastreiareceita.com/Logo500x100.svg"
+    }
+]
